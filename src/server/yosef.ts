b0010217c7cfa9ecfd299d19c88c6e4102ce5d50
yosefsahalo <yosefsahalu@gmail.com>
2022-12-06 07:53:27 +0200

    nodemailer function

--- src/server/yosef.ts
+++ src/server/yosef.ts
@@ -1,9 +1,18 @@
 import { Remult } from "remult";
+import { callRivhit, RivhitDocument } from "../app/create-invoice/invoice";
 import { getDataProvider } from "./getDataProvider";
 import { sendMail } from "./sendMail";
 
 getDataProvider().then(async dp => {
   const remult = new Remult();
   remult.setDataProvider(dp);
-  await sendMail(789, 4);
+//   const result: { document_list: RivhitDocument[] } = await callRivhit("Document.List", {
+//    // from_date: dateToString(lastDate),
+//   //  to_date: dateToString(new Date()),
+//     from_document_type: 1,
+//     to_document_type: 11
+
+// });
+// console.log(result);
+  await sendMail(98, 1);
 });
